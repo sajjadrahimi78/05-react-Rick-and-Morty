3e88bd6abbf2d9489705107adbf9304dd8e3f18e
Sajjad Rahimi <sajjad021.5555@gmail.com>
2024-05-27 21:16:26 +0330

    render Navbar component in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,115 +1,12 @@
-import { useEffect, useState } from "react";
 import "./App.css";
-import CharacterDetail from "./components/CharacterDetail";
-import CharacterList from "./components/CharacterList";
-import Navbar, { Favourites, Search, SearchResult } from "./components/Navbar";
-import { Toaster, toast } from "react-hot-toast";
-import axios from "axios";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [characters, setCharacters] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [query, setQuery] = useState("");
-  const [selectedId, setSelectedId] = useState(null);
-  const [favourites, setFavourites] = useState(
-    () => JSON.parse(localStorage.getItem("FAVOURITES")) || []
-  );
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    const controller = new AbortController();
-    const signal = controller.signal;
-    async function fetchData() {
-      try {
-        setIsLoading(true);
-        const { data } = await axios.get(
-          `https://rickandmortyapi.com/api/character/?name=${query}`,
-          { signal }
-        );
-        setCharacters(data.results.slice(0, 5));
-      } catch (err) {
-        // fetch => err.name ==="AbortError"
-        // axios => axios.isCancel()
-        if (!axios.isCancel()) {
-          setCharacters([]);
-          toast.error(err.response.data.error);
-        }
-      } finally {
-        setIsLoading(false);
-      }
-    }
-
-    // if (query.length < 3) {
-    //   setCharacters([]);
-    //   return;
-    // }
-
-    fetchData();
-
-    return () => {
-      controller.abort();
-    };
-  }, [query]);
-
-  useEffect(() => {
-    const interval = setInterval(() => setCount((c) => c + 1), 1000);
-
-    // return function(){}
-    return () => {
-      clearInterval(interval);
-    };
-  }, [count]);
-
-  useEffect(() => {
-    localStorage.setItem("FAVOURITES", JSON.stringify(favourites));
-  }, [favourites]);
-
-  // console.log(JSON.parse(localStorage.getItem("FAVOURITES")));
-
-  const handleSelectCharacter = (id) => {
-    setSelectedId((prevId) => (prevId === id ? null : id));
-  };
-
-  const handleAddFavourite = (char) => {
-    setFavourites((preFav) => [...preFav, char]);
-  };
-
-  const handleDeleteFavourite = (id) => {
-    setFavourites((preFav) => preFav.filter((fav) => fav.id !== id));
-  };
-
-  const isAddToFavourite = favourites.map((fav) => fav.id).includes(selectedId);
-
   return (
     <div className="app">
-      <Toaster />
-      <Navbar>
-        <Search query={query} setQuery={setQuery} />
-        <SearchResult numOfResult={characters.length} />
-        <Favourites
-          favourites={favourites}
-          onDeleteFavourite={handleDeleteFavourite}
-        />
-      </Navbar>
-      <Main>
-        <CharacterList
-          selectedId={selectedId}
-          characters={characters}
-          isLoading={isLoading}
-          onSelectCharacter={handleSelectCharacter}
-        />
-        <CharacterDetail
-          selectedId={selectedId}
-          onAddFavourite={handleAddFavourite}
-          isAddToFavourite={isAddToFavourite}
-        />
-      </Main>
+      <Navbar />
     </div>
   );
 }
 
 export default App;
-
-function Main({ children }) {
-  return <div className="main">{children}</div>;
-}
