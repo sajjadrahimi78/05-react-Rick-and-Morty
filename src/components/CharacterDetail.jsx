a47edf5e21519ab034a1307375a54977d8dbda49
Sajjad Rahimi <sajjad021.5555@gmail.com>
2024-05-27 23:29:49 +0330

    complited character-detail

--- src/components/CharacterDetail.jsx
+++ src/components/CharacterDetail.jsx
@@ -1,9 +1,38 @@
-
+import { character } from "../../data/data";
 
 function CharacterDetail() {
   return (
-    <div>CharacterDetail</div>
-  )
+    <div style={{ flex: 1 }}>
+      <div className="character-detail">
+        <img
+          src={character.image}
+          alt={character.name}
+          className="character-detail__img"
+        />
+        <div className="character-detail__info">
+          <h3 className="name">
+            <span>{character.gender === "Male" ? "🧔🏻‍♂️" : "👱🏻‍♀️"}</span>
+            <span>&nbsp;{character.name}</span>
+          </h3>
+          <div className="info">
+            <span
+              className={`status ${character.status === "Dead" ? "red" : ""}`}
+            ></span>
+            <span>&nbsp;{character.status}</span>
+            <span> - {character.species}</span>
+          </div>
+          <div className="location">
+            <p>Last known location:</p>
+            <p>{character.location.name}</p>
+          </div>
+          <div className="actions">
+            <button className="btn btn--primary">Add to Favourit</button>
+          </div>
+        </div>
+      </div>
+      <div className="character-episodes"></div>
+    </div>
+  );
 }
 
-export default CharacterDetail
+export default CharacterDetail;
