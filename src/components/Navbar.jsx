ee0fbd0e6a9a1a7f61b0c4fea7c913a0bf104ae0
Sajjad Rahimi <sajjad021.5555@gmail.com>
2024-05-27 21:15:46 +0330

    create Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,60 +1,18 @@
-import { HeartIcon, TrashIcon } from "@heroicons/react/24/outline";
-import { useState } from "react";
-import Modal from "./Modal";
-import { Character } from "./CharacterList";
+import React from 'react'
+import { HeartIcon } from '@heroicons/react/24/outline'
 
-export default function Navbar({ children }) {
+function Navbar() {
   return (
-    <nav className="navbar">
-      <Logo />
-      {children}
-    </nav>
-  );
+    <div className='navbar'>
+        <div className="navbar__logo">😍 LOGO</div>
+        <input type="text" name="" id="" className='text-field' placeholder='search...'/>
+        <div className="navbar__result">Found X characters</div>
+        <button className='heart'>
+            <HeartIcon className='icon'/>
+            <span className='badge'>4</span>
+        </button>
+    </div>
+  )
 }
 
-function Logo() {
-  return <div className="navbar__logo">LOGO 😍</div>;
-}
-
-export function Search({ query, setQuery }) {
-  return (
-    <input
-      value={query}
-      onChange={(e) => setQuery(e.target.value)}
-      type="text"
-      className="text-field"
-      placeholder="search..."
-    />
-  );
-}
-
-export function SearchResult({ numOfResult }) {
-  return <div className="navbar__result">Found {numOfResult} characters</div>;
-}
-
-export function Favourites({ favourites, onDeleteFavourite }) {
-  const [isOpen, setIsOpen] = useState(false);
-
-  return (
-    <>
-      <Modal onOpen={setIsOpen} open={isOpen} title="List of Favourites">
-        {favourites.map((item) => (
-          <Character key={item.id} item={item}>
-            <button
-              className="icon red"
-              onClick={() => onDeleteFavourite(item.id)}
-            >
-              <TrashIcon />
-            </button>
-          </Character>
-        ))}
-      </Modal>
-      <button className="heart" onClick={() => setIsOpen((is) => !is)}>
-        <HeartIcon className="icon" />
-        <span className="badge">{favourites.length}</span>
-      </button>
-    </>
-  );
-}
-
-// App => Navbar => SearchResult
+export default Navbar
