8a6ef1080924b0d015b207eef62594dfa372d652
Sajjad Rahimi <sajjad021.5555@gmail.com>
2024-05-27 22:16:41 +0330

    create CharacterList component det static api in data file create Character component and render in CharacterList

--- src/components/CharacterList.jsx
+++ src/components/CharacterList.jsx
@@ -1,31 +1,10 @@
-import { EyeIcon, EyeSlashIcon } from "@heroicons/react/24/outline";
-import { useState } from "react";
-import Loader from "./Loader";
+import { EyeIcon } from "@heroicons/react/24/outline";
 
-function CharacterList({
-  selectedId,
-  characters,
-  isLoading,
-  onSelectCharacter,
-}) {
-  if (isLoading)
-    return (
-      <div className="characters-list">
-        <Loader />
-      </div>
-    );
-
+function CharacterList({ character }) {
   return (
     <div className="characters-list">
-      {characters.map((item) => (
-        <Character key={item.id} item={item}>
-          <button
-            className="icon red"
-            onClick={() => onSelectCharacter(item.id)}
-          >
-            {selectedId === item.id ? <EyeSlashIcon /> : <EyeIcon />}
-          </button>
-        </Character>
+      {character.map((item) => (
+        <Character key={item.id} item={item} />
       ))}
     </div>
   );
@@ -33,38 +12,24 @@ function CharacterList({
 
 export default CharacterList;
 
-export function Character({ item, children }) {
+function Character({ item }) {
   return (
     <div className="list__item">
       <img src={item.image} alt={item.name} />
-      <CharaterName item={item} />
-      <CharacterInfo item={item} />
-      {children}
+      <h3 className="name">
+        <span>{item.gender === "Male" ? "🧔🏻‍♂️" : "👱🏻‍♀️"}</span>
+        <span> {item.name}</span>
+      </h3>
+      <div className="list-item__info info">
+        <span
+          className={`status ${item.status === "Dead" ? "red" : ""}`}
+        ></span>
+        <span> {item.status}</span>
+        <span> - {item.species}</span>
+      </div>
+      <button className="icon red">
+        <EyeIcon/>
+      </button>
     </div>
   );
 }
-
-function CharaterName({ item }) {
-  return (
-    <h3 className="name">
-      <span>{item.gender === "Male" ? "👱🏻‍♂️" : "👩🏻‍🦳"}</span>
-      <span> {item.name}</span>
-    </h3>
-  );
-}
-
-function CharacterInfo({ item }) {
-  return (
-    <div className="list-item__info info">
-      <span className={`status ${item.status === "Dead" ? "red" : ""}`}></span>
-      <span> {item.status} </span>
-      <span> - {item.species}</span>
-    </div>
-  );
-}
-
-// render logic : => pure render logic !!!
-// do not perform network req.
-// do not create time
-// do not access DOM API ...
-// do not mutate object, state, props !
