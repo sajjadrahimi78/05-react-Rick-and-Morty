e515715f226c7db39d1c2193a70fcc7692beb24c
Sajjad Rahimi <sajjad021.5555@gmail.com>
2024-05-27 22:17:59 +0330

    render CharacterList & CharacterDetail component in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { allCharacters } from "../data/data";
 import "./App.css";
+import CharacterDetail from "./components/CharacterDetail";
+import CharacterList from "./components/CharacterList";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="app">
       <Navbar />
+      <div className="main">
+        <CharacterList character={allCharacters}/>
+        <CharacterDetail/>
+      </div>
     </div>
   );
 }
