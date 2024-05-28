09e1342ce9100e1ecbcadff91d18d9b8ca1197ec
Sajjad Rahimi <sajjad021.5555@gmail.com>
2024-05-28 10:47:42 +0330

    create state characters create CharacterName  & CharacterInfo and renders in Character

--- src/components/CharacterList.jsx
+++ src/components/CharacterList.jsx
@@ -1,9 +1,13 @@
 import { EyeIcon } from "@heroicons/react/24/outline";
+import { allCharacters } from "../../data/data";
+import { useState } from "react";
 
-function CharacterList({ character }) {
+function CharacterList() {
+  // state
+  const [characters, setCharacters] = useState(allCharacters);
   return (
     <div className="characters-list">
-      {character.map((item) => (
+      {characters.map((item) => (
         <Character key={item.id} item={item} />
       ))}
     </div>
@@ -16,20 +20,30 @@ function Character({ item }) {
   return (
     <div className="list__item">
       <img src={item.image} alt={item.name} />
-      <h3 className="name">
-        <span>{item.gender === "Male" ? "🧔🏻‍♂️" : "👱🏻‍♀️"}</span>
-        <span> {item.name}</span>
-      </h3>
-      <div className="list-item__info info">
-        <span
-          className={`status ${item.status === "Dead" ? "red" : ""}`}
-        ></span>
-        <span>&nbsp;{item.status}</span>
-        <span> - {item.species}</span>
-      </div>
+      <CharacterName item={item} />
+      <CharacterInfo item={item}/>
       <button className="icon red">
         <EyeIcon />
       </button>
+    </div>
+  );
+}
+
+function CharacterName({ item }) {
+  return (
+    <h3 className="name">
+      <span>{item.gender === "Male" ? "🧔🏻‍♂️" : "👱🏻‍♀️"}</span>
+      <span> {item.name}</span>
+    </h3>
+  );
+}
+
+function CharacterInfo({ item }) {
+  return (
+    <div className="list-item__info info">
+      <span className={`status ${item.status === "Dead" ? "red" : ""}`}></span>
+      <span>&nbsp;{item.status}</span>
+      <span> - {item.species}</span>
     </div>
   );
 }
