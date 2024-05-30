349f405c4074de94890dd6cbf7eddc8dd2b9ec35
Sajjad Rahimi <sajjad021.5555@gmail.com>
2024-05-31 01:11:53 +0330

    update Loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,9 +1,20 @@
 
+import { LoaderIcon } from "react-hot-toast";
 
 function Loader() {
   return (
-    <p>Loading data...</p>
-  )
+    <div
+      style={{
+        color: "var(--slate-300)",
+        display: "flex",
+        alignItems: "center",
+        gap: "1rem",
+      }}
+    >
+     <p> Loading Data...</p>
+      <LoaderIcon style={{ width: "1.3rem", height: "1.3rem" }} />
+    </div>
+  );
 }
 
-export default Loader
+export default Loader;
